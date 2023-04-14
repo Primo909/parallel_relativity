84989dc81655785f949e5c2d79d6d99da62683aa
Kevin Steiner <kevin.steiner@hotmail.de>
2023-04-14 13:49:48 +0100

    edited runscripts and gitignore

--- pointConvTest.sh
+++
@@ -1,8 +0,0 @@
-file="parallel_main.exe"
-echo "Cleaning Data folder"
-make cleanFIG > /dev/null
-echo "Making executable"
-make $file > /dev/null
-echo "Running simulation"
-mpiexec -np 4 bin/$file 10 0 1 > /dev/null
-python3 pyt/pointConvergence.py
--- run_serial.sh
+++
@@ -1,9 +0,0 @@
-file="bin/main.exe"
-
-echo "control,N,time"
-
-for N in 1000 2000 3000 4000 5000
-do
-	for i in {1..10}; do $file $N | grep control; done
-done
-
