8ac484b9a7e83c60502c34d01982e6372e021204
Kevin Steiner <kevin.steiner@hotmail.de>
2023-04-14 17:05:55 +0100

    fixed compare script

--- compare.sh
+++ compare.sh
@@ -8,17 +8,17 @@ make $exe > /dev/null
 
 run_simulation () {
 echo "control,N,size,time"
-for cores in 1 2 3 
+for cores in 1 2
 do
 	for i in {1..2} 
 	do 
-		make cleanFIG #> /dev/null
-		mpiexec -np $cores $file $N 1 0 | grep control
+		make cleanFIG > /dev/null
+		mpiexec -np $cores $file $N $1 0 | grep control
 	done
 done
 }
 
 echo "Running the simulstion on 4 cores"
-run_simulation "0" | tee Data/time_no_save.csv
-run_simulation "1" | tee Data/time_yes_save.csv
+run_simulation 1 | tee Data/time_yes_save.csv
+run_simulation 0 | tee Data/time_no_save.csv
 python3 pyt/compare.py
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+user_function () {
+         echo "user_function $1 $2 $3 $4"
+}
+user_function 3
+
