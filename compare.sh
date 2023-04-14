be736c5ae1f9322d86744cc60511cc3f3794c1ff
Kevin Steiner <kevin.steiner@hotmail.de>
2023-04-14 16:41:00 +0100

    added compare script

---
+++ compare.sh
@@ -0,0 +1,21 @@
+#file="./bin/parallel_main.exe"
+
+file="bin/parallel_main.exe"
+exe="parallel_main.exe"
+N="1000"
+echo "Making executable"
+make $exe > /dev/null
+
+run_simulation () {
+echo "control,N,size,time"
+for cores in 1 2
+do
+	for i in {1..5}; do mpiexec -np $cores $file $N $1 0 | grep control;
+	done
+done
+}
+
+echo "Running the simulstion on 4 cores"
+run_simulation "0" | tee Data/time_no_save.csv
+run_simulation "1" | tee Data/time_yes_save.csv
+python3 pyt/compare.py
---
+++ pyt/compare.py
@@ -0,0 +1,60 @@
+# here, serial execution time is considered as single-core run of the parallelized code
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+lis = ["Data/time_no_save.csv","Data/time_yes_save.csv"]
+description = ["no ouptup", "with output"]
+
+plt.rc('axes', labelsize=16) 
+fig, ax = plt.subplots() 
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel("# cores")
+plt.ylabel("Speedup")
+
+for j in range(2) :
+    file = lis[j]
+    df = pd.read_csv(file).drop(columns=["control"])
+    df =  df.groupby(by=["size","N"],as_index=False).mean()
+
+    # calculating speedup, eficiency for each combination of (N, size)
+    
+    for i in df["N"].unique():
+    		temp = df.copy().loc[df["N"]==i]
+    		single_core = temp.loc[df["size"]==1]["time"].values
+    		speedup = single_core/temp["time"]
+    		plt.plot(temp["size"], speedup, marker=".", label=description[j])
+    
+    
+    plt.ylim(1, 2.8)
+    plt.legend()
+
+x = np.linspace(1,4,100)
+plt.plot(x, x, "k", label="$S_n$ = n")  # line of X = X
+plt.show()
+
+
+''' efficiency
+fig, ax = plt.subplots()
+#plt.rc('axes', labelsize=16) 
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel("#cores")
+plt.ylabel("Efficiency")
+
+for i in df["N"].unique():
+		temp = df.copy().loc[df["N"]==i]
+		#size = temp["size"]
+		single_core = temp.loc[df["size"]==1]["time"].values
+		speedup = single_core/temp["time"]
+		plt.plot(temp["size"], speedup/temp["size"], marker=".", label="N = " + str(i))
+
+# labels and limits
+ax.xaxis.set_major_locator(MaxNLocator(integer=True)) # x-axis with integer values only
+plt.legend()
+plt.savefig('pyt/Figures/efficiency.png')
+plt.show()
+'''
